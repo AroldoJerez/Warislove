ca9a59d4b5a8e75d721c9327f3daced029ea07db
Aroldo Jerez <107219476+AroldoJerez@users.noreply.github.com>
2024-06-11 20:08:33 -0300

    Iniciando
    Empezando warislove

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<section className="flex min-h-screen items-center justify-center bg-slate-700 text-6xl font-serif">
+<a className="text-white">WARISLOVE</a>
+<image></image>
+  </section>
+  );
+}
