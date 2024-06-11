492852c0a62df962dd57544da923271d050ec11b
Aroldo Jerez <107219476+AroldoJerez@users.noreply.github.com>
2024-06-11 20:26:10 -0300

    agregando logo
    logo agregado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import logo from "../../public/logo.png"
 
 export default function Home() {
   return (
 <section className="flex min-h-screen items-center justify-center bg-slate-700 text-6xl font-serif">
 <a className="text-white">WARISLOVE</a>
-<image></image>
+<Image
+      src={logo}
+      width={500}
+      height={500}
+      alt="Picture of the author"></Image>
   </section>
   );
 }
