5688632616035aeb7cba4755f5eba0e6fc529906
Aroldo Jerez <107219476+AroldoJerez@users.noreply.github.com>
2024-07-02 11:49:21 -0300

    configurando roles
    configurando roles

---
+++ prisma/migrations/20240702130918_roles/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `admin` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "guild" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT DEFAULT 'member',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("_id", "createdAt", "email", "guild", "password", "updatedAt", "username") SELECT "_id", "createdAt", "email", "guild", "password", "updatedAt", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
